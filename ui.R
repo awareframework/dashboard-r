8f12b0cb801c216ceeb4b2684ced557a86e7ba64
Vassilis Kostakos <vkostakos@users.noreply.github.com>
2017-06-01 16:57:29 +1000

    Tweaks

--- ui.R
+++ ui.R
@@ -15,19 +15,13 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
 
-      
+      # new tab content
       tabItem(tabName = "settings" ,
-              h2("Connection settings"),
-              p("Please enter the details of your database server, and click CONNECT."),
-              textInput(options()$mysql$host, "Host", value = "awareframework.com", width = NULL, placeholder = ""),
-              textInput(options()$mysql$port, "Port", value = "3306", width = NULL, placeholder = ""),
-              textInput(options()$mysql$user, "Username", value = "Hosio_235", width = NULL, placeholder = ""),
-              passwordInput(options()$mysql$password, "Password", value = "", width = NULL, placeholder = ""),
-              textInput(options()$mysql$database, "Database", value = "Hosio_235", width = NULL, placeholder = "")
+              tabSettingsUI("tabSettings")
       ),
       
       
-      # First tab content
+      # new tab content
       tabItem(tabName = "dashboard",
               h2("Overview of all data"),
               fluidRow(
